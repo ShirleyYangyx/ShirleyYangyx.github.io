eb8266ba318d7bb1faa4ce1663acce9c7aed6e39
ZhengYeah <zhengyeah@foxmail.com>
2026-03-06 21:29:19 -0500

    Codex optimize

--- _showcase/paintings/watercolor_2.md
+++ _showcase/paintings/watercolor_2.md
@@ -5,5 +5,5 @@ date: 2020-01-12 00:01:00 +0800
 group: Watercolor
 ---
 <div>
-    <img data-src="{{ 'assets/images/etc/watercolor2.jpeg' | relative_url }}" class="lazy w-100 rounded-xl" src="{{ '/assets/images/etc/watercolor2.jpeg' | relative_url }}">
+    <img data-src="{{ '/assets/images/etc/watercolor2-opt.jpg' | relative_url }}" class="lazy w-100 rounded-xl" src="{{ '/assets/images/empty_300x200.png' | relative_url }}" alt="Watercolor artwork">
 </div>
--- assets/js/common.js
+++ assets/js/common.js
@@ -1,42 +1,66 @@
 // aHR0cHM6Ly9naXRodWIuY29tL2x1b3N0MjYvYWNhZGVtaWMtaG9tZXBhZ2U=
 $(function () {
-    lazyLoadOptions = {
-        scrollDirection: 'vertical',
-        effect: 'fadeIn',
-        effectTime: 300,
-        placeholder: "",
-        onError: function(element) {
-            console.log('[lazyload] Error loading ' + element.data('src'));
-        },
-        afterLoad: function(element) {
-            if (element.is('img')) {
-                // remove background-image style
-                element.css('background-image', 'none');
-                element.css('min-height', '0');
-            } else if (element.is('div')) {
-                // set the style to background-size: cover; 
-                element.css('background-size', 'cover');
-                element.css('background-position', 'center');
+    var hasLazyPlugin = typeof $.fn.Lazy === 'function';
+    var hasMasonryPlugin = typeof $.fn.masonry === 'function';
+    var hasImagesLoaded = typeof $.fn.imagesLoaded === 'function';
+
+    if (hasLazyPlugin) {
+        var lazyLoadOptions = {
+            scrollDirection: 'vertical',
+            effect: 'fadeIn',
+            effectTime: 300,
+            placeholder: "",
+            onError: function(element) {
+                console.log('[lazyload] Error loading ' + element.data('src'));
+            },
+            afterLoad: function(element) {
+                if (element.is('img')) {
+                    // remove background-image style
+                    element.css('background-image', 'none');
+                    element.css('min-height', '0');
+                } else if (element.is('div')) {
+                    // set the style to background-size: cover;
+                    element.css('background-size', 'cover');
+                    element.css('background-position', 'center');
+                }
             }
-        }
+        };
+
+        $('img.lazy, div.lazy:not(.always-load)').Lazy({visibleOnly: true, ...lazyLoadOptions});
+        $('div.lazy.always-load').Lazy({visibleOnly: false, ...lazyLoadOptions});
+    } else {
+        // Fallback: ensure lazy-marked images still load when plugin is not enabled for this page.
+        $('img.lazy[data-src]').each(function() {
+            var $img = $(this);
+            if (!$img.attr('src') || $img.attr('src').indexOf('empty_300x200.png') !== -1) {
+                $img.attr('src', $img.data('src'));
+            }
+        });
+        $('div.lazy[data-src]').each(function() {
+            var $div = $(this);
+            $div.css('background-image', 'url("' + $div.data('src') + '")');
+            $div.css('background-size', 'cover');
+            $div.css('background-position', 'center');
+        });
     }
 
-    $('img.lazy, div.lazy:not(.always-load)').Lazy({visibleOnly: true, ...lazyLoadOptions});
-    $('div.lazy.always-load').Lazy({visibleOnly: false, ...lazyLoadOptions});
+    $('[data-toggle="tooltip"]').tooltip();
 
-    $('[data-toggle="tooltip"]').tooltip()
+    if (hasMasonryPlugin && $('.grid').length > 0) {
+        var $grid = $('.grid').masonry({
+            "percentPosition": true,
+            "itemSelector": ".grid-item",
+            "columnWidth": ".grid-sizer"
+        });
+        // layout Masonry after each image loads
+        if (hasImagesLoaded) {
+            $grid.imagesLoaded().progress(function () {
+                $grid.masonry('layout');
+            });
+        }
 
-    var $grid = $('.grid').masonry({
-        "percentPosition": true,
-        "itemSelector": ".grid-item",
-        "columnWidth": ".grid-sizer"
-    });
-    // layout Masonry after each image loads
-    $grid.imagesLoaded().progress(function () {
-        $grid.masonry('layout');
-    });
-
-    $(".lazy").on("load", function () {
-        $grid.masonry('layout');
-    });
-})
+        $(".lazy").on("load", function () {
+            $grid.masonry('layout');
+        });
+    }
+});
